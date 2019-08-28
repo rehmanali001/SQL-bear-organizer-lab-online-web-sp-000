470385c9c2d2cc3b8ba5db2a3ac277a1d5ae398c
Rehman Ali <rehmanali001@gmail.com>
2019-08-28 22:39:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT color, COUNT(color) FROM bears WHERE color = 'dark brown';"
 end
